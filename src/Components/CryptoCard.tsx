0cda980ebf8a0c3772620240a8dcdeee443d6d5c
Leonardo Sebastian Gauto <112297703+LeoSebastian23@users.noreply.github.com>
2024-07-12 21:14:23 -0300

    Update cards
    delete LineChart

--- src/Components/CryptoCard.tsx
+++ src/Components/CryptoCard.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import LineChart from "./LineChart"; // Ajusta la ruta según donde esté tu componente LineChart
-
 interface CryptoCardProps {
   coin: {
     image: string;
@@ -8,13 +6,11 @@ interface CryptoCardProps {
     symbol: string;
     price_change_percentage_24h: number;
     current_price: number;
-    price_history?: number[];  // Historial de precios para el gráfico
   };
   index: number;
 }
 
 const CryptoCard: React.FC<CryptoCardProps> = ({ coin, index }) => {
-  // Función para formatear el precio
   const formatPrice = (price: number) => {
     return price.toLocaleString('en-US', {
       minimumFractionDigits: 2,
@@ -22,16 +18,12 @@ const CryptoCard: React.FC<CryptoCardProps> = ({ coin, index }) => {
     }).replace('.', '#').replace(',', '.').replace('#', ',');
   };
 
-  const history = coin.price_history || [];
-
   return (
     <div
       className="card mb-3 p-2 text-bg-dark crypto-card"
       style={{ width: "18rem", color: "#343a40" }}
     >
       <div className="d-flex justify-center align-items-center">
-    <div className="card mb-3 p-2 text-bg-dark crypto-card">
-      <div className="d-flex justify-content-between align-items-center">
         <img
           src={coin.image}
           className="card-img-top"
@@ -45,11 +37,6 @@ const CryptoCard: React.FC<CryptoCardProps> = ({ coin, index }) => {
       </div>
       <div className="card-body">
 
-        <h5 className="card-title">
-          {coin.name}
-          <span className="text-muted text-uppercase">({coin.symbol})</span>
-        </h5>
-        <LineChart data={history} />
         <div className="d-flex justify-content-between align-items-center">
           <p className="fw-semibold mb-0">
             ${formatPrice(coin.current_price)}
